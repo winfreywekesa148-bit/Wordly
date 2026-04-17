e64048dd8dd44da7dd3603373f1b0eeae99f4809
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-04-17 13:25:02 +0300

    Initialize script.js with DOM element references

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('searchform');
+const input = document.getElementById('input');
+const result = document.getElementById('result');
+const audio = document.getElementById('audio');
+const description = document.getElementById('description');
+const synonyms = document.getElementById('synonyms');
